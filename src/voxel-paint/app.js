9da2ff05cd52bf54703d784b24f7565dc2647c58
Gonster <gs5647@gmail.com>
2015-03-02 01:14:43 +0800

    correct error

--- src/voxel-paint/app.js
+++ src/voxel-paint/app.js
@@ -676,7 +676,7 @@ AV.initialize("i5m1bad33f8bm725g0lan5wd8hhc1c4qhyz3cyq4b0qoyvja", "2w44ugxt0z512
                     for(var i = 0, l = results.length;i < l; i++) {
                         $('#openModal .modal-body .button-group-vertical').append(
                           '<label class="btn btn-primary">'
-                          + '  <input type="radio" name="boxData" id="boxData'+i+'" value="'+objectId+'" autocomplete="off">'
+                          + '  <input type="radio" name="boxData" id="boxData'+i+'" value="'+results[i].objectId+'" autocomplete="off">'
                           + '  <label id="boxDataLabel'+i+'"></label>'
                           + '</label>'
                         );
