39db368db881416765332455030b7e2e66fb4b1a
Gonster <gs5647@gmail.com>
2015-03-02 01:34:37 +0800

    ~

--- src/voxel-paint/app.js
+++ src/voxel-paint/app.js
@@ -676,7 +676,7 @@ AV.initialize("i5m1bad33f8bm725g0lan5wd8hhc1c4qhyz3cyq4b0qoyvja", "2w44ugxt0z512
                           + '  <label id="boxDataLabel'+i+'"></label>'
                           + '</label>'
                         );
-                        $('#openModal .modal-body .button-group-vertical #boxDataLabel'+i).text(results[i].get('name');
+                        $('#openModal .modal-body .button-group-vertical #boxDataLabel'+i).text(results[i].get('name'));
                     }
                     $('#openModal').modal('show');
                 }
