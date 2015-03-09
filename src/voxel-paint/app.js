0db43dc53c744de8b33f1c0fb14c92a223f0a2d4
Gonster <gs5647@gmail.com>
2015-03-09 20:40:41 +0800

    test criteria

--- src/voxel-paint/app.js
+++ src/voxel-paint/app.js
@@ -950,7 +950,7 @@ AV.initialize("i5m1bad33f8bm725g0lan5wd8hhc1c4qhyz3cyq4b0qoyvja", "2w44ugxt0z512
             query.equalTo('user', AV.User.current());
 
             var query2 = new AV.Query(Keep);
-            query2.select('name');
+            // query2.select('name');
             query2.include(['box.name', 'box.user.name']);
             query2.equalTo('user', AV.User.current());
 
@@ -976,7 +976,7 @@ AV.initialize("i5m1bad33f8bm725g0lan5wd8hhc1c4qhyz3cyq4b0qoyvja", "2w44ugxt0z512
             query.equalTo('user', AV.User.current());
 
             var query2 = new AV.Query(Keep);
-            query2.select('name');
+            // query2.select('name');
             query2.include(['box.name', 'box.user.name']);
             query2.equalTo('user', AV.User.current());
 
@@ -1033,7 +1033,7 @@ AV.initialize("i5m1bad33f8bm725g0lan5wd8hhc1c4qhyz3cyq4b0qoyvja", "2w44ugxt0z512
           query.equalTo('user', AV.User.current());
 
           var query2 = new AV.Query(Keep);
-          query2.select('name');
+          // query2.select('name');
           query2.include(['box.name', 'box.user.name']);
           query2.equalTo('user', AV.User.current());
 
